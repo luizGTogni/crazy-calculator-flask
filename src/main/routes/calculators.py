3c661174aa74229b1e19bb216d33a8484ce39b17
Luiz Togni <contato.togni@gmail.com>
2025-07-22 16:43:43 -0300

    feat: calculator_1 route

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_blueprint = Blueprint("calc_routes", __name__)
 
 @calc_route_blueprint.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    print(request)
+    calc = Calculator1()
+    calc.calculate(request)
     return jsonify({ "success": True }), 200
