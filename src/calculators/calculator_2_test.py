2549b129a89a1bf117dd4bf5c19dbb20c0115647
Luiz Togni <contato.togni@gmail.com>
2025-07-23 17:01:29 -0300

    refactor: change Numpyhandler to MockDriverHandler

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,15 +1,28 @@
-from typing import Dict
+from typing import Dict, List
 from .calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers: List[float]) -> float:
+        return 3
+
+def test_calculate_integration():
     mock_request = MockRequest({ "numbers": [2.12, 4.62, 1.32] })
     calculator_2 = Calculator2(driver_handler=NumpyHandler())
     response = calculator_2.calculate(mock_request)
 
     assert isinstance(response, dict)
     assert response == {"data": {"Calculator": 2, "result": 0.08}}
+
+def test_calculate():
+    mock_request = MockRequest({ "numbers": [2.12, 4.62, 1.32] })
+    calculator_2 = Calculator2(driver_handler=MockDriverHandler())
+    response = calculator_2.calculate(mock_request)
+
+    assert isinstance(response, dict)
+    assert response == {"data": {"Calculator": 2, "result": 0.33}}
