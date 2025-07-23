520215f57857e2068be7291cc4132caf5592e457
Luiz Togni <contato.togni@gmail.com>
2025-07-23 16:33:12 -0300

    feat: add calculator2 unit test

---
+++ src/calculators/calculator_2_test.py
@@ -0,0 +1,14 @@
+from typing import Dict
+from .calculator_2 import Calculator2
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+def test_calculate():
+    mock_request = MockRequest({ "numbers": [2.12, 4.62, 1.32] })
+    calculator_2 = Calculator2()
+    response = calculator_2.calculate(mock_request)
+
+    assert isinstance(response, dict)
+    assert response == {"data": {"Calculator": 2, "result": 0.08}}
