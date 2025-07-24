8357c497a4f868bc4cf51415c32d2ff15a4f1692
Luiz Togni <contato.togni@gmail.com>
2025-07-24 11:32:54 -0300

    feat: add custom errors

---
+++ src/errors/error_controller.py
@@ -0,0 +1,24 @@
+from .http_unprocessable_entity import HttpUnprocessableEntityError
+from .http_bad_request import HttpBadRequestError
+
+def handle_error(error: Exception):
+    if isinstance(error, (HttpUnprocessableEntityError, HttpBadRequestError)):
+        return {
+            "status_code": error.status_code,
+            "body": {
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        }
+    
+    return {
+        "status_code": 500,
+        "body": {
+            "errors": [{
+                "title": "Server Error",
+                "detail": str(error)
+            }]
+        }
+    }
