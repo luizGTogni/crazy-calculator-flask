0947b41894878067f6aea5bd054451dda944ea6a
Luiz Togni <contato.togni@gmail.com>
2025-07-23 22:44:47 -0300

    feat: add calculator3 route

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.main.factories.calculator1_factory import calculator1_factory
 from src.main.factories.calculator2_factory import calculator2_factory
+from src.main.factories.calculator3_factory import calculator3_factory
 
 calc_route_blueprint = Blueprint("calc_routes", __name__)
 
@@ -11,8 +12,16 @@ def calculator_1():
     response = calc.calculate(request)
     return jsonify(response), 200
 
+
 @calc_route_blueprint.route("/calculator/2", methods=["POST"])
 def calculator_2():
     calc = calculator2_factory()
+    response = calc.calculate(request)
+    return jsonify(response), 200
+
+
+@calc_route_blueprint.route("/calculators/3", methods=["POST"])
+def calculator_3():
+    calc = calculator3_factory()
     response = calc.calculate(request)
     return jsonify(response), 200
