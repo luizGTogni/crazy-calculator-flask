0b3728460046296ddee8ebc1e595abda826a89c8
Luiz Togni <contato.togni@gmail.com>
2025-07-23 22:43:32 -0300

    feat: add calculator3 class

---
+++ src/calculators/calculator_3.py
@@ -0,0 +1,42 @@
+from typing import Dict, List
+from flask import Request
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+
+
+class Calculator3:
+    def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+        self.__driver_handler = driver_handler
+
+    def calculate(self, request: Request) -> Dict:
+        body = request.json
+        input_data = self.__validate_body(body)
+        variance = self.__calculate_variance(input_data)
+        multiplication = self.__calculate_multiplication(input_data)
+        self.__verify_results(variance, multiplication)
+        response = self.__format_response(variance)
+        return response
+
+    def __validate_body(self, body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("body invalid")
+
+        input_data = body["numbers"]
+        return input_data
+
+    def __calculate_variance(self, numbers: List[float]) -> float:
+        variance = self.__driver_handler.variance(numbers)
+        return variance
+
+    def __calculate_multiplication(self, numbers: List[float]) -> float:
+        multiplication = 1
+        for n in numbers:
+            multiplication *= n
+
+        return multiplication
+
+    def __verify_results(self, variance: float, multiplication: float) -> None:
+        if variance < multiplication:
+            raise Exception("Process failed")
+
+    def __format_response(self, variance: float) -> Dict:
+        return {"data": {"Calculator": 3, "value": variance, "success": True}}
