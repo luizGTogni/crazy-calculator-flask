b1ec67a083e5ed1b11201d9030d7a9c5e121f499
Luiz Togni <contato.togni@gmail.com>
2025-07-24 11:33:32 -0300

    refactor: change generic error to custom error

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,6 +1,8 @@
 from typing import Dict, List
 from flask import Request
 from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
+from src.errors.http_bad_request import HttpBadRequestError
 
 
 class Calculator3:
@@ -18,7 +20,7 @@ class Calculator3:
 
     def __validate_body(self, body: Dict) -> List[float]:
         if "numbers" not in body:
-            raise Exception("body invalid")
+            raise HttpUnprocessableEntityError("body invalid")
 
         input_data = body["numbers"]
         return input_data
@@ -36,7 +38,7 @@ class Calculator3:
 
     def __verify_results(self, variance: float, multiplication: float) -> None:
         if variance < multiplication:
-            raise Exception("Process failed")
+            raise HttpBadRequestError("Process failed")
 
     def __format_response(self, variance: float) -> Dict:
         return {"data": {"Calculator": 3, "value": variance, "success": True}}
