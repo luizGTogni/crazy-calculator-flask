66e7be880c4d36d0be672e0dd732f21a34e0d0eb
Luiz Togni <contato.togni@gmail.com>
2025-07-24 12:08:56 -0300

    feat: add mean numpy method

--- src/drivers/numpy_handler.py
+++ src/drivers/numpy_handler.py
@@ -12,3 +12,6 @@ class NumpyHandler(DriverHandlerInterface):
 
     def variance(self, numbers: List[float]) -> float:
         return self.__np.var(numbers)
+    
+    def mean(self, numbers: List[float]) -> float:
+        return self.__np.mean(numbers)
